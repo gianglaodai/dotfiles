ff97c851654359accf14d83e78033a12186a6981
giang hoang <13800000+gianglaodai@users.noreply.github.com>
2024-07-07 21:44:50 +0700

    add image

--- .config/nvim/lua/user/core/options.lua
+++ .config/nvim/lua/user/core/options.lua
@@ -1,46 +1,46 @@
 local options = {
-	background = "light",
-	backup = false,
-	clipboard = "unnamedplus",
-	cmdheight = 2,
-	completeopt = { "menuone", "noselect" },
-	conceallevel = 0,
-	fileencoding = "utf-8",
-	hlsearch = true,
-	ignorecase = true,
-	mouse = "a",
-	pumheight = 10,
-	showmode = false,
-	showtabline = 2,
-	smartcase = true,
-	smartindent = true,
-	splitbelow = true,
-	splitright = true,
-	swapfile = false,
-	timeoutlen = 1000,
-	undofile = true,
-	updatetime = 300,
-	writebackup = false,
-	expandtab = true,
-	shiftwidth = 2,
-	tabstop = 2,
-	cursorline = true,
-	number = true,
-	relativenumber = true,
-	numberwidth = 4,
-	signcolumn = "yes",
-	wrap = false,
-	scrolloff = 8,
-	sidescrolloff = 8,
-	guifont = "ComicShannsMono Nerd Font Mono:h14",
+  background = "light",
+  backup = false,
+  clipboard = "unnamedplus",
+  cmdheight = 2,
+  completeopt = { "menuone", "noselect" },
+  conceallevel = 0,
+  fileencoding = "utf-8",
+  hlsearch = true,
+  ignorecase = true,
+  mouse = "a",
+  pumheight = 10,
+  showmode = false,
+  showtabline = 2,
+  smartcase = true,
+  smartindent = true,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  timeoutlen = 1000,
+  undofile = true,
+  updatetime = 300,
+  writebackup = false,
+  expandtab = true,
+  shiftwidth = 2,
+  tabstop = 2,
+  cursorline = true,
+  number = true,
+  relativenumber = true,
+  numberwidth = 4,
+  signcolumn = "yes",
+  wrap = false,
+  scrolloff = 8,
+  sidescrolloff = 8,
+  guifont = "ComicShannsMono Nerd Font Mono:h14",
 }
-vim.g.neovide_transparency = 0.8 
-vim.g.transparency = 1
+vim.g.neovide_transparency = 0.8
+vim.g.transparency = 0.5
 
 vim.opt.shortmess:append("c")
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+  vim.opt[k] = v
 end
 
 vim.cmd("set whichwrap+=<,>,[,],h,l")
